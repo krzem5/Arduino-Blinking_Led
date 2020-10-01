9bb36c1b783f5e6a20fcc5901a9e80294943e9e9
Neerajkry <32574772+Neerajkry@users.noreply.github.com>
2020-10-01 14:25:13 +0530

    Update index.ino

--- index.ino
+++ index.ino
@@ -14,3 +14,33 @@ void loop(){
 	digitalWrite(12,LOW);
 	delay(550);
 }
+
+
+
+
+
+/*
+Use this code and work with the help of on board led and without any delay 
+
+const int ledPin =  13;
+int ledState = LOW;            
+long previousMillis = 0;        
+long interval = 1000;
+void setup() {
+  pinMode(ledPin, OUTPUT);      
+}
+ 
+void loop()
+{
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis;   
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+    digitalWrite(ledPin, ledState);
+  }
+}
+*/
